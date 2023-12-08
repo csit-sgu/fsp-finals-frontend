4d35dbfb5c3b3fe93f61c5bd6a2bd54ab96c119a
Artem Kudyakov <t3m8ch@yandex.ru>
2023-12-09 00:02:13 +0400

    feat(creating quiz): Bind fields

--- src/pages/CreatingQuiz/components/QuizMetadataForm.tsx
+++ src/pages/CreatingQuiz/components/QuizMetadataForm.tsx
@@ -11,9 +11,25 @@ export const QuizMetadataForm = ({ value, onChange }: QuizMetadataFormProps) => 
   return (
     <>
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <TextField label="Название" sx={{ marginBottom: '5px' }} />
-        <TextField label="Описание" sx={{ marginBottom: '5px' }} multiline rows={3} />
-        <TextField label="Категория" />
+        <TextField
+          label="Название"
+          sx={{ marginBottom: '5px' }}
+          value={value.title}
+          onChange={(e) => onChange({ ...value, title: e.target.value })}
+        />
+        <TextField
+          label="Описание"
+          sx={{ marginBottom: '5px' }}
+          multiline
+          rows={3}
+          value={value.description}
+          onChange={(e) => onChange({ ...value, description: e.target.value })}
+        />
+        <TextField
+          label="Категория"
+          value={value.category}
+          onChange={(e) => onChange({ ...value, category: e.target.value })}
+        />
       </Box>
     </>
   );
