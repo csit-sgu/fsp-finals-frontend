f0fab4975dc21920108f5ddab944d19cd7fad115
Artem Kudyakov <t3m8ch@yandex.ru>
2023-12-08 12:49:10 +0400

    feat: Make layout for login and register pages

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,5 +1,28 @@
-import { Typography } from '@mui/material';
+import { TextField, Box, Typography, Button } from '@mui/material';
 
 export const LoginPage = () => {
-  return <Typography variant="h2">Login</Typography>;
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        component="form"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '500px',
+        }}
+      >
+        <Typography variant="h2">Вход</Typography>
+        <TextField variant="outlined" label="Имя пользователя" sx={{ marginBottom: '10px' }} />
+        <TextField variant="outlined" label="Пароль" sx={{ marginBottom: '10px' }} />
+        <Button variant="contained">Войти</Button>
+      </Box>
+    </div>
+  );
 };
--- src/pages/Register/RegisterPage.tsx
+++ src/pages/Register/RegisterPage.tsx
@@ -1,5 +1,30 @@
-import { Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 export const RegisterPage = () => {
-  return <Typography variant="h2">Register</Typography>;
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        component="form"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '500px',
+        }}
+      >
+        <Typography variant="h2">Регистрация</Typography>
+        <TextField variant="outlined" label="Имя и фамилия" sx={{ marginBottom: '10px' }} />
+        <TextField variant="outlined" label="Логин" sx={{ marginBottom: '10px' }} />
+        <TextField variant="outlined" label="Пароль" sx={{ marginBottom: '10px' }} />
+        <TextField variant="outlined" label="Повторите пароль" sx={{ marginBottom: '10px' }} />
+        <Button variant="contained">Зарегистрироваться</Button>
+      </Box>
+    </div>
+  );
 };
