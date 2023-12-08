81208c179dc51bbc58e09ac750088d899df0277a
Artem Kudyakov <t3m8ch@yandex.ru>
2023-12-08 12:25:25 +0400

    feat: Setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { HomePage } from './pages/Home/HomePage';
+import { LoginPage } from './pages/Login/LoginPage';
+import { RegisterPage } from './pages/Register/RegisterPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
