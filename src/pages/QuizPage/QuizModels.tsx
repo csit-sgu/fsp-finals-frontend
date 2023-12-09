6f2b8366030586432a5445af0cfeef00af79c584
Andrew Guschin <guschin.drew@gmail.com>
2023-12-09 09:41:05 +0300

    Add theme

--- src/pages/QuizPage/QuizModels.tsx
+++ src/pages/QuizPage/QuizModels.tsx
@@ -2,17 +2,6 @@ export type QuizId = string | number;
 export type BlockId = string | number | null;
 export type AuthorId = string | number;
 
-// {
-//   "quiz_id": "0069cedf-1149-4073-a6bb-991dc4b16ca7",
-//   "title": "Aboba quiz",
-//   "author_id": "e3129b36-d325-4465-a4ab-d59b79ffb323",
-//   "description": "This quiz will check your knowledge of aboba",
-//   "category": "web",
-//   "entry_id": "dce15e45-3929-41db-8d3b-03cf58601e7e",
-//   "age_group": "adult",
-//   "complexity": 3
-// }
-
 export interface QuizBackend {
   quiz_id: QuizId;
   title: string;
